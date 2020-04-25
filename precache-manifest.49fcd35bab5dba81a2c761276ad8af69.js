3db207f2d890855d2959709552075c31f44b5794
Frank Wisniewski <me@frankwisniewski.net>
2020-04-25 08:13:10 -0500

    Updates

---
+++ precache-manifest.49fcd35bab5dba81a2c761276ad8af69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f9c7b5e260cf50f12656334d7ba2afa",
+    "url": "/telimed/index.html"
+  },
+  {
+    "revision": "acd8fd4ce88a04867097",
+    "url": "/telimed/static/css/main.c7b63297.chunk.css"
+  },
+  {
+    "revision": "136ee2322ec950bbb116",
+    "url": "/telimed/static/js/2.42b7c940.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/telimed/static/js/2.42b7c940.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd8fd4ce88a04867097",
+    "url": "/telimed/static/js/main.a8548e53.chunk.js"
+  },
+  {
+    "revision": "785c79a9f30cf96abfd2",
+    "url": "/telimed/static/js/runtime-main.ab74ab6f.js"
+  }
+]);
